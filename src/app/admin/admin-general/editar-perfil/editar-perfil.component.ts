afcc363023f5420b03f3806aee9d62ca63993733
Pancito <pansiendopan@gmail.com>
2025-06-16 19:21:46 -0500

    16dejunio

--- src/app/admin/admin-general/editar-perfil/editar-perfil.component.ts
+++ src/app/admin/admin-general/editar-perfil/editar-perfil.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-editar-perfil',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './editar-perfil.component.html',
   styleUrl: './editar-perfil.component.css'
 })
