7d89245b15d76d8b25b83860604c3c0dd95c7147
Pancito <pansiendopan@gmail.com>
2025-11-24 19:51:33 -0500

    contacto con bd6

--- src/app/cliente/cliente-contacto/cliente-contacto.component.ts
+++ src/app/cliente/cliente-contacto/cliente-contacto.component.ts
@@ -3,6 +3,7 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { ChatbotComponent } from '../chatbot/chatbot.component';
 import { CommonModule } from '@angular/common';
 import { DatoSastreriaService } from '../../shared/services/dato-sastreria.service';
+import { DatoSastreria } from '../../models/dato-sastreria.model';
 
 @Component({
   selector: 'app-cliente-contacto',
@@ -12,14 +13,25 @@ import { DatoSastreriaService } from '../../shared/services/dato-sastreria.servi
   styleUrl: './cliente-contacto.component.css'
 })
 export class ClienteContactoComponent implements OnInit {
-  datos: any = null;
+  contacto: DatoSastreria = {
+    idDatos: 0,
+    nombre: '',
+    telefono: '',
+    direccion: '',
+    descripcion: '',
+    logoSastreria: ''
+  };
 
   constructor(private datoService: DatoSastreriaService) {}
 
   ngOnInit(): void {
     this.datoService.getDato().subscribe({
-      next: (res) => this.datos = res,
-      error: (err) => console.log(err)
+      next: (data) => {
+        if (data.length > 0) {
+          this.contacto = data[0];
+        }
+      },
+      error: (err) => console.error('Error al cargar datos', err)
     });
   }
 }
