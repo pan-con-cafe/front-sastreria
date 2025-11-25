8267b6346ce64126bb705f61448f944e18f9e5f2
Pancito <pansiendopan@gmail.com>
2025-11-24 19:01:56 -0500

    contacto con bd

--- src/app/cliente/cliente-contacto/cliente-contacto.component.ts
+++ src/app/cliente/cliente-contacto/cliente-contacto.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ChatbotComponent } from '../chatbot/chatbot.component';
+import { CommonModule } from '@angular/common';
+import { DatoSastreriaService } from '../../shared/services/dato-sastreria.service';
 
 @Component({
   selector: 'app-cliente-contacto',
   standalone: true,
-  imports: [NavbarComponent, ChatbotComponent],
+  imports: [NavbarComponent, ChatbotComponent, CommonModule],
   templateUrl: './cliente-contacto.component.html',
   styleUrl: './cliente-contacto.component.css'
 })
-export class ClienteContactoComponent {
+export class ClienteContactoComponent implements OnInit {
+  datos: any = null;
 
+  constructor(private datoService: DatoSastreriaService) {}
+
+  ngOnInit(): void {
+    this.datoService.obtenerDatos().subscribe({
+      next: (res) => this.datos = res,
+      error: (err) => console.log(err)
+    });
+  }
 }
---
+++ src/app/shared/services/dato-sastreria.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatoSastreriaService {
+
+  private apiUrl = 'https://sastreria-estilo-ljge.onrender.com/api/datosastreria';
+
+  constructor(private http: HttpClient) {}
+
+  obtenerDatos(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/obtener`);
+  }
+}
